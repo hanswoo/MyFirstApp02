27f115dac50c95e3ff5a496dfc1fdfe7f19fb062
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-03-08 11:35:53 +0900

    Initial Commit

---
+++ MyFirstApp02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp02
+//
+//  Created by D7702_10 on 2018. 3. 8..
+//  Copyright © 2018년 hanswoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
